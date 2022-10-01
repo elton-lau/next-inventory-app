9317c29f3c8a0895bb3aafd90e7af5e0da43944f
Elton Lau <twilightlau94@gmail.com>
2022-10-01 05:19:34 -0400

    completed signin function

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import { FaGoogle } from 'react-icons/fa';
+import {Center, Button, Title, Stack, Text, Group} from '@mantine/core';
+
+import { supabase } from '../../utils/supabaseClient';
+
+
+
+interface SignInProps {}
+
+const SignIn: FC<SignInProps> = ({}) => {
+
+  async function signInWithGoogle(event: any) {
+    event.preventDefault();
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'google',
+    })
+  }
+
+    return (
+      <>
+        <Center sx={{width: '100vw', height: '100vh'}}>
+          <Stack spacing="xl">
+            <Title align="center">Welcome To Inventory App ✋</Title>
+            <Button onClick={signInWithGoogle} size="lg" sx={{alignSelf: 'center'}}>
+              <Group>
+                <Text size="md">Sign in With Google</Text>
+                <FaGoogle />
+              </Group>
+            </Button>
+          </Stack>
+        </Center>
+      </>
+    );
+}
+export default SignIn;
